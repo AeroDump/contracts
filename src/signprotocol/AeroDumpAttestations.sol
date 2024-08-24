20c3463c9449e8475f3f58251a7373f0c3efc621
Riiz0 <shawnrizo@hotmail.com>
2024-08-24 12:36:23 -0400

    Feat: Initial AeroDumpAttestation Contract

---
+++ src/signprotocol/AeroDumpAttestations.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ISPHook } from "@signprotocol/signprotocol-evm/src/interfaces/ISPHook.sol";
+
+contract AeroDumpAttestations is Ownable, ISPHook {
+    constructor(address initialOwner) Ownable(initialOwner) { }
+
+    function didReceiveAttestation(
+        address attester,
+        uint64 schemaId,
+        uint64 attestationId,
+        bytes calldata extraData
+    )
+        external
+        payable
+        override
+    { }
+
+    function didReceiveAttestation(
+        address attester,
+        uint64 schemaId,
+        uint64 attestationId,
+        IERC20 resolverFeeERC20Token,
+        uint256 resolverFeeERC20Amount,
+        bytes calldata extraData
+    )
+        external
+        override
+    { }
+
+    function didReceiveRevocation(
+        address attester,
+        uint64 schemaId,
+        uint64 attestationId,
+        bytes calldata extraData
+    )
+        external
+        payable
+        override
+    { }
+
+    function didReceiveRevocation(
+        address attester,
+        uint64 schemaId,
+        uint64 attestationId,
+        IERC20 resolverFeeERC20Token,
+        uint256 resolverFeeERC20Amount,
+        bytes calldata extraData
+    )
+        external
+        override
+    { }
+}
