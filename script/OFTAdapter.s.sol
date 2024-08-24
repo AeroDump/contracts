53c578b0208f4dbcdcba5c0a190b21fa195ceeb3
Jaskaran Singh <jaskaransingh@Jaskarans-Laptop.local>
2024-08-24 21:44:38 +0530

    install

---
+++ script/OFTAdapter.s.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+import {OFTAdapter} from "../src/OFTAdapter.sol";
+
+contract OFTAdapterScript is Script {
+    // OFTAdapter adapter = new OFTAdapter(ad);
+}
