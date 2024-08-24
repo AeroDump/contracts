3b15d4aa02ad4ac6bbd4c86061a1d9542ac4c3b1
Riiz0 <shawnrizo@hotmail.com>
2024-08-24 19:03:45 -0400

    Reformat: AeroDumpAttestations Contract Logic

--- src/signprotocol/AeroDumpAttestations.sol
+++ src/signprotocol/AeroDumpAttestations.sol
@@ -7,34 +7,41 @@ import { ISP } from "@signprotocol/signprotocol-evm/src/interfaces/ISP.sol";
 import { Attestation } from "@signprotocol/signprotocol-evm/src/models/Attestation.sol";
 import { DataLocation } from "@signprotocol/signprotocol-evm/src/models/DataLocation.sol";
 
-contract AeroDump is Ownable {
+contract AeroDumpAttestations is Ownable {
     ISP public spInstance;
 
-    error ProjectAlreadyRegistered();
     error NotAuthorizedToVerify();
-    error ProjectNotRegisrtered();
-    error NotProjectOwner();
-    error ProjectNotVerified();
-    error NoRefundAgreement();
 
-    struct Project {
-        address projectOwner;
-        bool isVerified;
-        bool hasRefundAgreement;
-    }
-
-    uint64 public projectVerificationSchemaId;
+    uint64 public projectSchemaId;
     uint64 public csvUploadSchemaId;
     uint64 public tokenDepositSchemaId;
     uint64 public userConsentSchemaId;
     uint64 public distributionCertificateSchemaId;
     uint64 public airdropExecutionSchemaId;
 
-    mapping(string => Project) private s_projects;
     mapping(address => bool) private s_verifiers;
 
     constructor(address initialOwner, address _spInstance) Ownable(initialOwner) {
         spInstance = ISP(_spInstance);
+    }
+
+    function setSchemaIds(
+        uint64 _projectSchemaId,
+        uint64 _csvUploadSchemaId,
+        uint64 _tokenDepositSchemaId,
+        uint64 _userConsentSchemaId,
+        uint64 _distributionCertificateSchemaId,
+        uint64 _airdropExecutionSchemaId
+    )
+        external
+        onlyOwner
+    {
+        projectSchemaId = _projectSchemaId;
+        csvUploadSchemaId = _csvUploadSchemaId;
+        tokenDepositSchemaId = _tokenDepositSchemaId;
+        userConsentSchemaId = _userConsentSchemaId;
+        distributionCertificateSchemaId = _distributionCertificateSchemaId;
+        airdropExecutionSchemaId = _airdropExecutionSchemaId;
     }
 
     function addVerifier(address verifier) external onlyOwner {
@@ -42,14 +49,11 @@ contract AeroDump is Ownable {
     }
 
     function registerProject(string memory projectName) external {
-        require(s_projects[projectName].projectOwner == address(0), ProjectAlreadyRegistered());
-        s_projects[projectName].projectOwner = msg.sender;
-
         bytes[] memory recipients = new bytes[](1);
         recipients[0] = abi.encode(msg.sender);
 
         Attestation memory a = Attestation({
-            schemaId: projectVerificationSchemaId,
+            schemaId: projectSchemaId,
             linkedAttestationId: 0,
             attestTimestamp: 0,
             revokeTimestamp: 0,
@@ -64,28 +68,86 @@ contract AeroDump is Ownable {
         spInstance.attest(a, "", "", "");
     }
 
-    function verifyProject(string memory projectName) external {
+    function verifyProject(string memory projectName, address projectOwner) external {
         require(s_verifiers[msg.sender], NotAuthorizedToVerify());
-        require(s_projects[projectName].projectOwner != address(0), ProjectNotRegisrtered());
 
-        s_projects[projectName].isVerified = true;
+        bytes[] memory recipients = new bytes[](1);
+        recipients[0] = abi.encode(projectOwner);
+
+        Attestation memory a = Attestation({
+            schemaId: projectSchemaId,
+            linkedAttestationId: 0,
+            attestTimestamp: 0,
+            revokeTimestamp: 0,
+            attester: address(this),
+            validUntil: 0,
+            dataLocation: DataLocation.ONCHAIN,
+            revoked: false,
+            recipients: recipients,
+            data: abi.encode(projectName, projectOwner, true, false) // name, owner, isVerified, hasRefundAgreement
+         });
+
+        spInstance.attest(a, "", "", "");
     }
 
-    function recordCSVFileUpload(bytes32 fileHash, uint256 recipientCount) external { }
+    function recordCSVFileUpload(string memory projectName, bytes32 fileHash, uint256 recipientCount) external {
+        bytes[] memory recipients = new bytes[](1);
+        recipients[0] = abi.encode(msg.sender);
+
+        Attestation memory a = Attestation({
+            schemaId: csvUploadSchemaId,
+            linkedAttestationId: 0,
+            attestTimestamp: 0,
+            revokeTimestamp: 0,
+            attester: address(this),
+            validUntil: 0,
+            dataLocation: DataLocation.ONCHAIN,
+            revoked: false,
+            recipients: recipients,
+            data: abi.encode(projectName, msg.sender, fileHash, block.timestamp, recipientCount)
+        });
+
+        spInstance.attest(a, "", "", "");
+    }
 
     function recordProjectTokenDeposit(string memory projectName, address tokenAddress, uint256 amount) external {
-        require(s_projects[projectName].projectOwner == msg.sender, NotProjectOwner());
+        bytes[] memory recipients = new bytes[](1);
+        recipients[0] = abi.encode(msg.sender);
+
+        Attestation memory a = Attestation({
+            schemaId: tokenDepositSchemaId,
+            linkedAttestationId: 0,
+            attestTimestamp: 0,
+            revokeTimestamp: 0,
+            attester: address(this),
+            validUntil: 0,
+            dataLocation: DataLocation.ONCHAIN,
+            revoked: false,
+            recipients: recipients,
+            data: abi.encode(projectName, msg.sender, tokenAddress, amount, block.timestamp)
+        });
+
+        spInstance.attest(a, "", "", "");
     }
 
-    function recordUserConsent(bool consentGiven) external { }
+    function recordUserConsent(bool consentGiven) external {
+        bytes[] memory recipients = new bytes[](1);
+        recipients[0] = abi.encode(msg.sender);
 
-    function recordNewsletterSent(string memory projectName, bytes32 messageHash, uint256 recipientCount) external {
-        require(s_projects[projectName].projectOwner == msg.sender, NotProjectOwner());
-    }
+        Attestation memory a = Attestation({
+            schemaId: userConsentSchemaId,
+            linkedAttestationId: 0,
+            attestTimestamp: 0,
+            revokeTimestamp: 0,
+            attester: address(this),
+            validUntil: 0,
+            dataLocation: DataLocation.ONCHAIN,
+            revoked: false,
+            recipients: recipients,
+            data: abi.encode(msg.sender, consentGiven, block.timestamp)
+        });
 
-    function signRefundAgreement(string memory projectName) external {
-        require(s_projects[projectName].projectOwner == msg.sender, NotProjectOwner());
-        s_projects[projectName].hasRefundAgreement = true;
+        spInstance.attest(a, "", "", "");
     }
 
     function issueDistributionCertificate(
@@ -95,10 +157,42 @@ contract AeroDump is Ownable {
     )
         external
     {
-        require(s_projects[projectName].projectOwner == msg.sender, NotProjectOwner());
-        require(s_projects[projectName].isVerified, ProjectNotVerified());
-        require(s_projects[projectName].hasRefundAgreement, NoRefundAgreement());
+        bytes[] memory recipients = new bytes[](1);
+        recipients[0] = abi.encode(msg.sender);
+
+        Attestation memory a = Attestation({
+            schemaId: distributionCertificateSchemaId,
+            linkedAttestationId: 0,
+            attestTimestamp: 0,
+            revokeTimestamp: 0,
+            attester: address(this),
+            validUntil: 0,
+            dataLocation: DataLocation.ONCHAIN,
+            revoked: false,
+            recipients: recipients,
+            data: abi.encode(projectName, msg.sender, totalAmount, recipientCount)
+        });
+
+        spInstance.attest(a, "", "", "");
     }
 
-    function recordAirdropTransaction(uint256 recipientCount, uint256 totalAmount, bytes32 transactionHash) external { }
+    function signRefundAgreement(string memory projectName) external {
+        bytes[] memory recipients = new bytes[](1);
+        recipients[0] = abi.encode(msg.sender);
+
+        Attestation memory a = Attestation({
+            schemaId: projectSchemaId,
+            linkedAttestationId: 0,
+            attestTimestamp: 0,
+            revokeTimestamp: 0,
+            attester: address(this),
+            validUntil: 0,
+            dataLocation: DataLocation.ONCHAIN,
+            revoked: false,
+            recipients: recipients,
+            data: abi.encode(projectName, msg.sender, true, true) // name, owner, isVerified, hasRefundAgreement
+         });
+
+        spInstance.attest(a, "", "", "");
+    }
 }
