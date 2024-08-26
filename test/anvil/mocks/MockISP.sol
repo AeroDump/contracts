0038bf7f97d286b762d242dacf58782601336935
Riiz0 <shawnrizo@hotmail.com>
2024-08-26 16:37:48 -0400

    Feat: Make Edits to MockISP Contract For Testing

--- test/anvil/mocks/MockISP.sol
+++ test/anvil/mocks/MockISP.sol
@@ -1,111 +1,80 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { ISP, Attestation, OffchainAttestation, Schema } from "@signprotocol/signprotocol-evm/src/interfaces/ISP.sol";
-import { ISPHook } from "@signprotocol/signprotocol-evm/src/interfaces/ISPHook.sol";
-import { DataLocation } from "@signprotocol/signprotocol-evm/src/models/DataLocation.sol";
+import { IVersionable } from "@signprotocol/signprotocol-evm/src/interfaces/IVersionable.sol";
+import { Schema } from "@signprotocol/signprotocol-evm/src/models/Schema.sol";
+import { Attestation, OffchainAttestation } from "@signprotocol/signprotocol-evm/src/models/Attestation.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract MockISP is ISP {
+contract MockISP is IVersionable {
+    mapping(uint64 => Schema) private _mockSchemas;
+    uint64 public schemaCounter = 1;
     bool public attestationMade;
     Attestation public lastAttestation;
-    mapping(uint64 => Schema) public schemas;
+    address[] public allowedAttesters;
 
-    function attest(
-        Attestation memory _attestation,
-        string memory _proofOfPersonhood,
-        string memory _proofOfOwnership,
-        string memory _extraData
-    )
-        external
-        override
-        returns (uint256)
-    {
-        attestationMade = true;
-        lastAttestation = _attestation;
-        return 1; // Return a dummy attestation ID
-    }
-
-    function attestMultiple(
-        Attestation[] memory _attestations,
-        string memory _proofOfPersonhood,
-        string memory _proofOfOwnership,
-        string memory _extraData
-    )
-        external
-        override
-        returns (uint256[] memory)
-    {
-        attestationMade = true;
-        if (_attestations.length > 0) {
-            lastAttestation = _attestations[0];
-        }
-        uint256[] memory ids = new uint256[](_attestations.length);
-        for (uint256 i = 0; i < _attestations.length; i++) {
-            ids[i] = i + 1; // Return dummy attestation IDs
-        }
-        return ids;
-    }
-
-    function revoke(uint256 _attestationId) external override {
-        // Implement if needed for testing
-    }
-
-    function getAttestation(uint256 _attestationId) external view override returns (Attestation memory) {
-        return lastAttestation;
-    }
-
-    function getAttestations(uint256[] memory _attestationIds) external view override returns (Attestation[] memory) {
-        Attestation[] memory attestations = new Attestation[](_attestationIds.length);
-        for (uint256 i = 0; i < _attestationIds.length; i++) {
-            attestations[i] = lastAttestation;
-        }
-        return attestations;
-    }
-
-    function getAttestationCount() external view override returns (uint256) {
-        return attestationMade ? 1 : 0;
-    }
-
-    function getSchema(uint64 _schemaId) external view override returns (Schema memory) {
-        return schemas[_schemaId];
-    }
-
-    function getSchemas(uint64[] memory _schemaIds) external view override returns (Schema[] memory) {
-        Schema[] memory schemasList = new Schema[](_schemaIds.length);
-        for (uint256 i = 0; i < _schemaIds.length; i++) {
-            schemasList[i] = schemas[_schemaIds[i]];
-        }
-        return schemasList;
-    }
-
-    function registerSchema(string memory _schema) external override returns (uint64) {
-        uint64 schemaId = uint64(schemas.length);
-        schemas[schemaId] = Schema({
-            registrant: address(this),
-            revocable: true,
-            dataLocation: DataLocation.STORED_ON_CHAIN,
-            maxValidFor: 0,
-            hook: ISPHook(address(0)),
-            timestamp: uint64(block.timestamp),
-            data: _schema
-        });
+    function register(Schema memory schema, bytes calldata delegateSignature) external returns (uint64 schemaId) {
+        schemaId = schemaCounter++;
+        _mockSchemas[schemaId] = schema;
         return schemaId;
     }
 
-    function supportTokenFactory(address _token, uint256 _tokenFactoryId) external override {
-        // Implement if needed for testing
+    function attest(
+        Attestation calldata attestation,
+        string calldata indexingKey,
+        bytes calldata delegateSignature,
+        bytes calldata extraData
+    )
+        external
+        returns (uint64 attestationId)
+    {
+        attestationMade = true;
+        lastAttestation = attestation;
+        return attestationId;
     }
 
-    function setAllowAll(bool _allowAll) external override {
-        // Implement if needed for testing
+    function revoke(
+        uint64 attestationId,
+        string calldata reason,
+        bytes calldata delegateSignature,
+        bytes calldata extraData
+    )
+        external
+    {
+        // No-op implementation
     }
 
-    function setAllowedAttester(address _attester, bool _allowed) external override {
-        // Implement if needed for testing
+    function getSchema(uint64 schemaId) external view returns (Schema memory) {
+        return _mockSchemas[schemaId];
     }
 
-    function isAllowedAttester(address _attester) external view override returns (bool) {
-        return true; // Always return true for testing purposes
+    function getAttestation(uint64 attestationId) external view returns (Attestation memory) {
+        return lastAttestation;
     }
+
+    function setAllowedAttester(address attester, bool allowed) external {
+        if (allowed) {
+            allowedAttesters.push(attester);
+        } else {
+            for (uint256 i = 0; i < allowedAttesters.length; i++) {
+                if (allowedAttesters[i] == attester) {
+                    delete allowedAttesters[i];
+                    break;
+                }
+            }
+        }
+    }
+
+    function isAllowedAttester(address attester) external view returns (bool) {
+        return contains(allowedAttesters, attester);
+    }
+
+    function contains(address[] memory array, address value) private pure returns (bool) {
+        for (uint256 i = 0; i < array.length; i++) {
+            if (array[i] == value) return true;
+        }
+        return false;
+    }
+
+    // Implement missing functions like attestBatch, revokeBatch, etc. based on the ISP interface
 }
